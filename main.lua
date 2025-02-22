ef711c4de0250d16a47be5ea8c81c039289f2a54
Alex Heidorn <heido023@d.umn.edu>
2025-02-22 17:29:06 -0600

    create lab tilset

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 function love.load()
     LoadMap("assets/map.txt")
+
+    -- lab tilset
+    local labQuadInfo = {
+        {' ', 0, 0},
+        {'*', 0, 32},
+        {'<', 32, 32},
+        {'^', 64, 32}
+    }
+    CreateMap(32, 32, "assets/tilesets/lab.png", quadInfo, "assets/maps/map.txt")
 end
 
 function love.update(dt)
