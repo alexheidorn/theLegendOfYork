32c6a00a907bb8ed0995769d874f8acf73cf8e73
Alex Heidorn <heido023@d.umn.edu>
2025-02-22 16:46:15 -0600

    hello worlf

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+    
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    love.graphics.print("Hello worlf!")
+end
+
+function love.quit()
+    return false
+end
