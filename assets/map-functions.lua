1e891afa2cb62b6165aabc99dd9c7da19c3c0ac1
Alex Heidorn <heido023@d.umn.edu>
2025-02-22 17:14:02 -0600

    draw map fxn

--- assets/map-functions.lua
+++ assets/map-functions.lua
@@ -1,44 +1,13 @@
-local tileTable, tileset, tileWidth, tileHeight, quads, quad
+local tileTable
 
-function LoadMap(path)
-    tileTable = {}
-    local file = love.filesystem.newFile(path)
-    for line in file:lines() do
-      table.insert(tileTable, line)
-    end
-    file:close()
-end
-
-function CreateMap(tileW, tileH, tileSetPath, quadInfo, tileString)
-    tileWidth = tileW
-    tileHeight = tileH
-    local sheetWidth, sheetHeight = tileset:getWidth(), tileset:getHeight()
-    
-    quads = {}
-    for i = 1, #quadInfo do
-      quads[i] = love.graphics.newQuad(quadInfo[i][1], quadInfo[i][2], quadInfo[i][3], quadInfo[i][4], tileset:getWidth(), tileset:getHeight())
-    end
-
-    tileTable = {}
-
-    for line in tileString:gmatch("[^\n]+") do
-        local row = {}
-        for char in line:gmatch(".") do
-          row[#row + 1] = char
-        end
-        tileTable[#tileTable + 1] = row
-      end
-
-end
-
-function DrawMap()
+function drawMap()
     for rowIndex = 1, #tileTable do
         local row = tileTable[rowIndex]
         for columnIndex = 1, #row do
           local number = row[columnIndex]
           local quad = quads[number]
           local x, y = (columnIndex - 1) * tileWidth, (rowIndex - 1) * tileHeight
-          love.graphics.draw(tileset, quad, x, y)
+          love.graphics.draw(Tileset, quad, x, y)
         end
     end
 end
