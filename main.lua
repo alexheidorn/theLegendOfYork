d9f3b8c31e64b8c0a1343f03692eb0fda6da74a4
Alex Heidorn <heido023@d.umn.edu>
2025-02-22 18:56:01 -0600

    add tilde as transparent tile

--- main.lua
+++ main.lua
@@ -8,7 +8,8 @@ function love.load()
         {' ', 0, 0},
         {'*', 0, 32},
         {'<', 32, 32},
-        {'^', 64, 32}
+        {'^', 64, 32},
+        {'~', 128, 0},
     }
     local mapTxtFile = love.filesystem.read('assets/maps/map.txt')
     CreateMap(32, 32, 'assets/tilesets/lab.png', labQuadInfo, mapTxtFile)
