cda965e930e4c238f1c41c1995c35008a297339c
Alex Heidorn <heido023@d.umn.edu>
2025-02-22 18:05:07 -0600

    actually import the other file...

--- main.lua
+++ main.lua
@@ -1,8 +1,17 @@
+require 'lib/map-functions'
+
 function love.load()
     -- LoadMap('/assets/maps/map.txt')
 
-    -- CreateMap(32, 32, '/assets/tilesets/lab.png', labQuadInfo, 'assets/maps/map.txt')
-    LoadMap('/assets/maps/map.lua')
+    -- lab tilset
+    local labQuadInfo = {
+        {' ', 0, 0},
+        {'*', 0, 32},
+        {'<', 32, 32},
+        {'^', 64, 32}
+    }
+    CreateMap(32, 32, 'assets/tilesets/lab.png', labQuadInfo, 'assets/maps/map.txt')
+    -- LoadMap('assets/maps/map.lua')
 end
 
 function love.update(dt)
