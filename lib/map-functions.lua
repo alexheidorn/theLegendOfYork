d7bb5e53c85dd3a3f6762b09c61bacf0e69cccc5
Alex Heidorn <heido023@d.umn.edu>
2025-02-22 18:20:36 -0600

    fix indents. add assert statement

--- lib/map-functions.lua
+++ lib/map-functions.lua
@@ -1,42 +1,43 @@
 local tileTable, tileset, tileWidth, tileHeight, quads, quad
 
 function LoadMap(path)
-    love.filesystem.load(path) ()
+  love.filesystem.load(path) ()
 end
 
 function CreateMap(tileW, tileH, tileSetPath, quadInfo, tileString)
-    tileset = love.graphics.newImage(tileSetPath)
-    tileWidth = tileW
-    tileHeight = tileH
-    local sheetWidth, sheetHeight = tileset:getWidth(), tileset:getHeight()
-    
-    quads = {}
-    for _, info in ipairs(quadInfo) do
-      -- info[1] = char, info[2] = x, info[3] = y
-      quads[info[1]] = love.graphics.newQuad(info[2], info[3], tileWidth, tileHeight, sheetWidth, sheetHeight)
+  tileset = love.graphics.newImage(tileSetPath)
+  tileWidth = tileW
+  tileHeight = tileH
+  local sheetWidth, sheetHeight = tileset:getWidth(), tileset:getHeight()
+
+  quads = {}
+  for _, info in ipairs(quadInfo) do
+    -- info[1] = char, info[2] = x, info[3] = y
+    quads[info[1]] = love.graphics.newQuad(info[2], info[3], tileWidth, tileHeight, sheetWidth, sheetHeight)
+  end
+
+
+  tileTable = {}
+
+  local mapWidth = #tileString:match("[^\n]+")
+  for line in tileString:gmatch("[^\n]+") do
+    assert(#line == mapWidth, 'Map is not aligned: width of row ' .. tostring(rowIndex) .. ' should be ' .. tostring(mapWidth) .. ', but it is ' .. tostring(#line))
+    local row = {}
+    for char in line:gmatch(".") do
+      row[#row + 1] = char
     end
-
-
-    tileTable = {}
-
-    for line in tileString:gmatch("[^\n]+") do
-        local row = {}
-        for char in line:gmatch(".") do
-          row[#row + 1] = char
-        end
-        tileTable[#tileTable + 1] = row
-      end
+    tileTable[#tileTable + 1] = row
+  end
 
 end
 
 function DrawMap()
-    for rowIndex = 1, #tileTable do
-        local row = tileTable[rowIndex]
-        for columnIndex = 1, #row do
-          local char = row[columnIndex]
-          local quad = quads[char]
-          local x, y = (columnIndex - 1) * tileWidth, (rowIndex - 1) * tileHeight
-          love.graphics.draw(tileset, quad, x, y)
-        end
+  for rowIndex = 1, #tileTable do
+      local row = tileTable[rowIndex]
+      for colIndex = 1, #row do
+        local char = row[colIndex]
+        local x, y = (colIndex - 1) * tileWidth, (rowIndex - 1) * tileHeight
+      love.graphics.draw(tileset, quads[char], x, y)
     end
+  end
 end
