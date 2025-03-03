ac4fe3c44157fa87a6a4442265d4a4fd5260d954
Alex Heidorn <heido023@d.umn.edu>
2025-03-03 09:49:41 -0600

    add ctor and placedholer update/draw fxns

--- resources/player.lua
+++ resources/player.lua
@@ -2,6 +2,22 @@ Entity = require 'entity'
 
 Player = Entity:extend()
 
+function Player:constructor(x, y)
+    self.x = x
+    self.y = y
+    self.health = 100
+    self.maxHealth = 100
+    self.attack = 10
+    self.defense = 10
+end
+
 function Player:new(x, y)
     Player.super.new(self, x, y)
+end
+
+function Player:update(dt)
+end
+
+function Player:draw()
+
 end
