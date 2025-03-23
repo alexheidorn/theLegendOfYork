f1864b77872604812dd6f79ff7c0d654e6b42de9
Alex Heidorn <heido023@d.umn.edu>
2025-03-23 17:10:53 -0500

    rmeove frameInfo. rely on frameCount for animations

--- lib/animation.lua
+++ lib/animation.lua
@@ -3,29 +3,29 @@
 
 Animation = Object:extend()
 
-function Animation:new(spriteSheet, frameWidth, frameHeight, frameInfo, frameCount, frameDuration)
+function Animation:new(spriteSheet, frameWidth, frameHeight, frameCount, frameDuration)
     self.spriteSheet = spriteSheet
     self.spritesheetWidth, self.spritesheetHeight = self.spriteSheet:getWidth(), self.spriteSheet:getHeight()
     self.frameWidth, self.frameHeight = frameWidth, frameHeight
 
-    -- -- Generate the frames (newQuads) based on spriteSheet dimensions and frame size
-    -- self.frames = {}
-    -- for i = 1, self.frameCount do
-    --     local x = (i - 1) * self.frameWidth
-    --     local y = 0
-    --     local quad = love.graphics.newQuad(x, y, self.frameWidth, self.frameHeight, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
-    --     table.insert(self.frames, quad)
-    -- end
-
+    -- Generate the frames (newQuads) based on spriteSheet dimensions and frame size
     self.frames = {}
-    for __, info in ipairs(frameInfo) do
-        local x, y = info[2], info[3]
+    for i = 1, frameCount do
+        local x = (i - 1) * self.frameWidth
+        local y = 0
         local quad = love.graphics.newQuad(x, y, self.frameWidth, self.frameHeight, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
         table.insert(self.frames, quad)
     end
 
+    -- self.frames = {}
+    -- for __, info in ipairs(frameInfo) do
+    --     local x, y = info[2], info[3]
+    --     local quad = love.graphics.newQuad(x, y, self.frameWidth, self.frameHeight, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
+    --     table.insert(self.frames, quad)
+    -- end
+
     self.frameDuration = frameDuration or 0.1 -- default to 0.1s per frame
-    self.frameCount = frameCount or #frameInfo or #self.frames
+    self.frameCount = frameCount or #self.frames
     self.currentFrame = 1
     self.activeFrame = self.frames[self.currentFrame]
     self.elapsedTime = 0
