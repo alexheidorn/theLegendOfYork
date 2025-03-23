bea20d9d023dde93c6b45b5c89ef9fffc2b3c7ae
Alex Heidorn <heido023@d.umn.edu>
2025-03-23 14:00:42 -0500

    move all animation and movement logic into entitiy

--- resources/entity.lua
+++ resources/entity.lua
@@ -1,13 +1,28 @@
 Object = require 'lib/classic'
 Entity = Object:extend()
 
-function Entity:new(x, y)
+function Entity:new(x, y, spriteSheet, frameWidth, frameHeight, frameDuration, frameCount)
     self.x = x
     self.y = y
+    self.speed = 60 -- Default speed in pixels per second
+
+    -- animation instance
+    self.spriteSheet = love.graphics.newImage(spriteSheet)
+    self.animation = Animation(self.spriteSheet, frameWidth, frameHeight, frameDuration, frameCount)
 end
 
 function Entity:update(dt)
+    self.animation:update(dt)
 end
 
-function Entity:draw()
+function Entity:move(moveX, moveY, dt)
+    local moveAmount = self.speed * dt
+    local newX = self.x + moveX * moveAmount
+    local newY = self.y + moveY * moveAmount
+    self.x = newX
+    self.y = newY
+end
+
+function Entity:draw() 
+    self.animation:draw(self.x, self.y)
 end
