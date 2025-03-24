0c642d44cdd42816d6a91d7d6211d74e239f6968
Alex Heidorn <heido023@d.umn.edu>
2025-03-24 18:40:23 -0500

    rename params/variables. fix collision code

--- resources/map.lua
+++ resources/map.lua
@@ -1,17 +1,20 @@
 Map = Object:extend()
 
-function Map:new(tileWidth, tileHeight, tileSetPath, quadInfo, tileString)
+function Map:new(tileSize,  tileSetPath, quadData, mapData, solidTiles)
     self.tileset = love.graphics.newImage(tileSetPath)
     self.tilesetWidth, self.tilesetHeight = self.tileset:getWidth(), self.tileset:getHeight()
-    self.tileWidth, self.tileHeight = tileWidth, tileHeight
+    self.tileSize = tileSize
+    self.solidTiles = solidTiles
+    
+
     self.quads = {}
-    for _, info in ipairs(quadInfo) do
+    for _, info in ipairs(quadData) do
         -- info[1] = char, info[2] = x, info[3] = y
-        self.quads[info[1]] = love.graphics.newQuad(info[2], info[3], self.tileWidth, self.tileHeight, self.tilesetWidth, self.tilesetHeight)
+        self.quads[info[1]] = love.graphics.newQuad(info[2], info[3], self.tileSize, self.tileSize, self.tilesetWidth, self.tilesetHeight)
     end
 
     self.tileTable = {}
-    for line in tileString:gmatch("[^\n]+") do
+    for line in mapData:gmatch("[^\n]+") do
         local row = {}
         for char in line:gmatch(".") do
             row[#row + 1] = char
@@ -25,34 +28,24 @@ function Map:update()
 end
 
 function Map:draw()
-    for y = 1, #self.tileTable do
-        for x = 1, #self.tileTable[y] do
-            local char = self.tileTable[y][x]
-            local quad = self.quads[char]
+    for row = 1, #self.tileTable do
+        for col = 1, #self.tileTable[row] do
+            local char = self.tileTable[row][col]
+            local quad = self.quads[char] or self.quads[' ']
             if quad then
-                love.graphics.draw(self.tileset, quad, (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
+                love.graphics.draw(self.tileset, quad, (col - 1) * self.tileSize, (row - 1) * self.tileSize)
             end
         end
     end
 end
 
-function Map:collides(x, y, width, height)
-    local tileX = math.floor(x / self.tileWidth) + 1
-    local tileY = math.floor(y / self.tileHeight) + 1
-    local tileWidth = math.ceil((x + width) / self.tileWidth) - tileX
-    local tileHeight = math.ceil((y + height) / self.tileHeight) - tileY
+function Map:collides(x, y)
+    local col = math.floor(x / self.tileSize) + 1
+    local row = math.floor(y / self.tileSize) + 1
 
-    for ty = tileY, tileY + tileHeight - 1 do
-        for tx = tileX, tileX + tileWidth - 1 do
-            local tile = self.tileTable[ty] and self.tileTable[ty][tx]
-            if tile then
-                local quad = self.quads[tile]
-                if quad then
-                    return true
-                end
-            end
-        end
+    if col < 1 or col > #self.tileTable[row] or row < 1 or row > #self.tileTable then
+        return true
     end
-
-    return false
+    local tile = self.tileTable[row][col]
+    return self.solidTiles[tile]
 end
