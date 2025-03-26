be593fdb82fe385e6f774e4fb1880465a9fca2dc
Alex Heidorn <alexheidorn12@gmail.com>
2025-03-26 16:59:39 -0500

    add hitbox & draw

--- resources/entity.lua
+++ resources/entity.lua
@@ -8,6 +8,8 @@ function Entity:new(x, y, spriteSheetPath, width, height, frameCount, frameDurat
     self.height = height or 32
     self.speed = 60 -- Default speed in pixels per second
 
+    self.showHitbox = false
+    self.hitbox = {x = 0, y = 0, width = self.width, height = self.height}
     -- animation instance
         -- codeium auto complete options from Balatro form
             -- self.sprite = Sprite(0, 0, 32, 32, G.ASSET_ATLAS['player'], {x = 0, y = 0})
@@ -29,11 +31,26 @@ function Entity:move(moveX, moveY, dt)
     if not G.map:collides(newX, newY, self.width, self.height) then
         self.x = newX
         self.y = newY
+        -- update hitbox position
+        self.hitbox.x = self.x + self.hitbox.offsetX
+        self.hitbox.y = self.y + self.hitbox.offsetY
     end
 end
 
 function Entity:draw()
+    -- draw the entity's sprite with animation
     self.animation:draw(self.x, self.y)
+
+    -- draw the entity's collision box
+    love.graphics.setColor(255, 0, 0, 128) -- Red with 50% transparency
+    love.graphics.rectangle(
+        "line", 
+        self.hitbox.x,
+        self.hitbox.y,
+        self.hitbox.width,
+        self.hitbox.height, 
+        8, 8) -- Rounded corners with radius 8
+    love.graphics.setColor(255, 255, 255, 255) -- Reset color
 end
 
 return Entity
