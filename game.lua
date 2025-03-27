c74dae01766f920023d4d1390568375f0fcb0711
Alex Heidorn <alexheidorn12@gmail.com>
2025-03-26 21:33:07 -0500

    store beginning spritesheets and tilset info

--- game.lua
+++ game.lua
@@ -1,9 +1,26 @@
 -- Class
 Game = Object:extend()
 
+function Game:new()
+    G = self
+
+    self:setGlobals()
+end
+
 function Game:load()
     -- initialize game componenets
-    Cam = Camera()
+
+    -- spritesheets
+    self.animation_atli = {
+        {name = "player", path = "assets/Zelda-like/character.png", pwidth = 32, pheight = 32, frames = 4, duration = 0.5},
+        {name = "log", path = "assets/Zelda-like/log.png", pwidth = 32, pheight = 32, frames = 4, duration = 0.5},
+    }
+    -- tilesets
+    self.asset_atlas = {
+        {name = "lab", path = "assets/tilesets/Untitled.png", pwidth = 32, pheight = 32},
+        {name = "overworld", path = "assets/Zelda-like/Overworld.png", pwidth = 32, pheight = 32},
+        {name = "inside", path = "assets/Zelda-like/Inner.png", pwidth = 16, pheight = 16},
+    }
 
     --Map creation logic
     -- lab tileset
@@ -135,5 +152,3 @@ function TitleScreenOptions()
     love.graphics.print("4) Exit :(", 100, 400)
 
 end
-
-G = Game()
