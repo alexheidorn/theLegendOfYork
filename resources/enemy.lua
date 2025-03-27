2fa646976df644ab381a01c73a4476bf11f6612c
Alex Heidorn <alexheidorn12@gmail.com>
2025-03-27 00:19:25 -0500

    add patorlling animation. update movement in correct order when detecting collision

--- resources/enemy.lua
+++ resources/enemy.lua
@@ -2,14 +2,25 @@ Enemy = Entity:extend()
 
 function Enemy:new(x, y)
     self.spriteSheetPath = "assets/Zelda-like/log.png"
-    Enemy.super.new(self, x, y, self.spriteSheetPath, 32, 32, 4)
+    self.animations = {
+        idle = { row = 1, frameCount = 4, loop = true, frameDuration = 0.5 },
+        patrolling = { row = 1, frameCount = 4, loop = true },
+    }
+    Enemy.super.new(self, x, y, self.spriteSheetPath, 32, 32, self.animations)
 
     self.state = "patrolling"
+    
     self.direction = 1 -- 1 = right, -1 = left
     self.timer = 0
 end
 
 function Enemy:update(dt)
+    local nextX = self.x + self.direction * self.speed * dt
+    if G.map:collides(nextX, self.y, self.width, self.height) then
+        -- change direction when colliding with a wall
+        self.direction = -self.direction
+        self.timer = 0
+    end
     -- simple AI movement: move back & forth
     self:move(self.direction, 0, dt)
 
@@ -21,12 +32,6 @@ function Enemy:update(dt)
             self.timer = 0
         end
     end
-
-    -- change direction when colliding with a wall
-    if G.map:collides(self.x, self.y, self.width, self.height) then
-        self.direction = -self.direction
-    end
-
     --update animation
     Enemy.super.update(self, dt)
 end
