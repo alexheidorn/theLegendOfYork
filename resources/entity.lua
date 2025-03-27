f4e0c7bc0ba5f6be369e119dbc80685d7cf4e0fb
Alex Heidorn <alexheidorn12@gmail.com>
2025-03-26 22:58:10 -0500

    create animations table & states. pass to animation. move & update hitboxes first. then map player position to hit box.

--- resources/entity.lua
+++ resources/entity.lua
@@ -1,24 +1,25 @@
 Object = require 'lib/classic'
 Entity = Object:extend()
 
-function Entity:new(x, y, spriteSheetPath, width, height, frameCount, frameDuration)
+function Entity:new(x, y, spriteSheetPath, width, height, animations)
     self.x = x or 0
     self.y = y or 0
     self.width = width or 32
     self.height = height or 32
     self.speed = 60 -- Default speed in pixels per second
+    self.animations = animations
+    self.state = ''
+    
 
     self.showHitbox = false
     self.hitbox = {x = 0, y = 0, width = self.width, height = self.height}
-    self.hitbox.offsetX = 0
-    self.hitbox.offsetY = 0
     -- animation instance
         -- codeium auto complete options from Balatro form
             -- self.sprite = Sprite(0, 0, 32, 32, G.ASSET_ATLAS['player'], {x = 0, y = 0})
             -- self.spriteSheet = love.graphics.newImage(G.ASSET_ATLAS['player'])
             -- self.spriteSheet = G.ASSET_ATLAS['player']
     self.spriteSheet = love.graphics.newImage(spriteSheetPath)
-    self.animation = Animation(self.spriteSheet, self.width, self.height, frameCount, frameDuration)
+    self.animation = Animation(self.spriteSheet, self.width, self.height, self.animations[self.state])
 end
 
 function Entity:update(dt)
@@ -31,17 +32,16 @@ function Entity:move(moveX, moveY, dt)
     -- Check X movement
     local newX = self.x + moveX * moveAmount
     if not G.map:collides(newX, self.y, self.width, self.height) then
-        self.x = newX
-        self.hitbox.x = self.x + self.hitbox.offsetX
+        self.hitbox.x = newX
+        self.x = self.hitbox.x
     end
 
     -- Check Y movement
     local newY = self.y + moveY * moveAmount
     if not G.map:collides(self.x, newY, self.width, self.height) then
-        self.y = newY
-        self.hitbox.y = self.y + self.hitbox.offsetY
+        self.hitbox.y = newY
+        self.y = self.hitbox.y
     end
-end
 end
 
 function Entity:draw()
