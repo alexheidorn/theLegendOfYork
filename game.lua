0946b31750c8175a2bd415183734ce1aa640ef7d
Alex Heidorn <alexheidorn12@gmail.com>
2025-03-30 22:56:47 -0500

    move camera components into cam.lua

--- game.lua
+++ game.lua
@@ -8,24 +8,10 @@ function Game:new()
 end
 
 function Game:load()
-    -- The game's graphics scale up, this method finds the right ratio
-    self.CAM = Camera()
-    -- 3 parameters: fullscreen, width, height
-    -- width and height are ignored if fullscreen is true
-    self.fullscreen = true
-    self.testWindow = false
-    self.vertical = false
-    self:setWindowSize(self.fullscreen, 1920, 1080)
-
-    if self.vertical then
-        self.fullscreen = false
-        self.testWindow = true
-        self:setWindowSize(self.fullscreen, 1360, 1920)
-    end
-
-    self:setScale()
     -- initialize game componenets
-    
+    self.CAM = Camera(0, 0, G.scale)
+    G.CAM.smoother = Camera.smooth.damped(8)
+    self:loadCam()
     local testMap = Map("lab")
     self.MAP = testMap
 
@@ -55,55 +41,6 @@ function Game:load()
     end
 end
 
-function Game:setWindowSize(full, width, height)
-    if full then
-        self.fullscreen = true
-        love.window.setFullscreen(true)
-        windowWidth = love.graphics.getWidth()
-        windowHeight = love.graphics.getHeight()
-    else
-        self.fullscreen = false
-        if width == nil or height == nil then
-            windowWidth = 1920
-            windowHeight = 1080
-        else
-            windowWidth = width
-            windowHeight = height
-        end
-        love.window.setMode( windowWidth, windowHeight, {resizable = not testWindow} )
-    end
-end
-
-function Game:setScale(input)
-    local windowHeight = love.graphics.getHeight()
-    self.scale = (7.3 / 1200) * windowHeight
-
-    if self.vertical then
-        self.scale = (7 / 1200) * windowHeight
-    end
-
-    if self.CAM then
-        self.CAM:zoomTo(self.scale)
-    end
-end
-
-function Game:checkWindowSize()
-    local width = love.graphics.getWidth()
-    local height = love.graphics.getHeight()
-    if width ~= windowWidth or height ~= windowHeight then
-        self:reinitSize()
-    end
-end
-
-function Game:reinitSize()
-    -- Reinitialize everything
-    windowWidth = love.graphics.getWidth()
-    windowHeight = love.graphics.getHeight()
-    G:setScale()
-    -- pause:init()
-    -- initFonts()
-end
-
 function Game:update(dt)
     if G.GAME_STATE == G.GAME_STATES.title_screen then
         -- Main menu logic here
@@ -120,32 +57,7 @@ function Game:update(dt)
         entity:update(dt)
     end
     
-    -- camera logic
-    local screenWidth = love.graphics.getWidth()
-    local screenHeight = love.graphics.getHeight()
-    local playerCenterX = self.PLAYER.hitbox.x + self.PLAYER.hitbox.width / 2
-    local playerCenterY = self.PLAYER.hitbox.y + self.PLAYER.hitbox.height / 2
-    self.CAM:lookAt(playerCenterX, playerCenterY)
-    
-    
-    -- Camera boundaries
-    -- Left Boundary
-    -- if Cam.x < screenWidth / 2 then
-    --     Cam.x = screenWidth / 2
-    -- end
-    -- -- Top Boundary
-    -- if Cam.y < screenHeight / 2 then
-    --     Cam.y = screenHeight / 2
-    -- end
-    -- -- Right Boundary
-    -- if Cam.x > self.map.width * self.map.tileSize - screenWidth / 2 then
-    --     Cam.x = self.map.width * self.map.tileSize - screenWidth / 2
-    -- end
-    -- -- Bottom Boundary
-    -- if Cam.y > self.map.height * self.map.tileSize - screenHeight / 2 then
-    --     Cam.y = self.map.height * self.map.tileSize - screenHeight / 2
-    -- end
-    
+    self.updateCam(dt)    
     self.INPUT:update(dt)
 end
 
