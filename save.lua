a74279d936f7d6dca2c3e7f39702c501ff387aed
Alex Heidorn <heido023@d.umn.edu>
2025-04-04 15:21:39 -0500

    write data using json encoder (working!)

--- save.lua
+++ save.lua
@@ -1,15 +1,22 @@
 Data = Object:extend()
+local json = require "lib/dkjson"
 
 function Data:new()
-    self.fileNumber = 0
-    self.playerName = "Player"
-    self.playTime = 0
-    self.saveDate = os.date("%Y-%m-%d")
-    self.saveTime = os.date("%H:%M:%S")
+    self.data = {
+        fileNumber = 0,
+        playerName = "Player",
+        playTime = 0,
+        saveDate = os.date("%Y-%m-%d"),
+        saveTime = os.date("%H:%M:%S"),
 
-    self.player = {}
-    self.enemies = {}
-    self.map = {}
+        player = {},
+        enemies = {},
+        map = {},
+    }
+end
+
+function Data:encode()
+    return json.encode(self.data, { indent = true})
 end
 
 function Data:createNewFile(fileNumber)
@@ -20,14 +27,14 @@ function Data:createNewFile(fileNumber)
 end
 
 function Data:save()
-    self.player = G.PLAYER
-    self.enemies = G.ENEMIES
-    self.map = G.MAP
+    self.data.player = G.PLAYER
+    self.data.enemies = G.ENEMIES
+    self.datamap = G.MAP
 
     -- save the game data to a file
-    local success, message = love.filesystem.write("save" .. self.fileNumber .. ".txt", love.data.serialize(self))
+    local success, message = love.filesystem.write("save" .. self.data.fileNumber .. ".txt", Data:encode())
     if success then
-        print("Game data saved to save" .. self.fileNumber .. ".txt")
+        print("Game data saved to save" .. self.data.fileNumber .. ".txt")
     else
         print("Error saving game data: " .. message)
     end
@@ -36,7 +43,7 @@ end
 function Data:load()
     -- load the game data from a file
     local data = love.filesystem.read("save" .. self.fileNumber .. ".txt")
-    return love.data.deserialize(data)
+    return love.data.unpack(data)
 end
 
 function Data:delete()
